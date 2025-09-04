194c4015fdca71f6a567feb0d10d364228f95c33
Ana Clara Oliveira Bezerra <anaclara00938@gmail.com>
2025-09-04 11:51:26 -0300

    [feat] Add Tarefas

--- App.js
+++ App.js
@@ -1,23 +1,38 @@
 import { StatusBar } from "expo-status-bar";
-import { Image, StyleSheet, Text, TextInput, View } from "react-native";
-import logo from "./assets/pngegg(1).png";
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View, Alert } from "react-native";
+import logo from "./assets/pngegg (1).png";
 import {useState} from "react";
-import btnAdd from "./assets/pngegg(2).png";
+import btnAdd from "./assets/btnAdd.png";
 
 export default function App() {
   const [tarefa, setTarefa] = useState("");
+  const [tarefas, setTarefas] = useState([])
+
+  const handleAdd = () => {
+
+    //Alert.alert(tarefa);
+    setTarefas([tarefa, ...tarefas]);
+    setTarefa("");
+
+  }
+
+//ja importei o flatlist só utilizar
+
   return (
     <View style={styles.container}>
       <View>
         <Image source={logo} style={styles.logo}/> 
         <Text>TODO List</Text>
         </View>
-     <View>
+     <View style={styles.viewInput}>
       <TextInput placeholder="Entre com a tarefa" 
        value={tarefa}
        onChangeText={setTarefa}
       />
-      <Image source={btnAdd}/>
+      <TouchableOpacity onPress={handleAdd}>
+        <Image source={btnAdd} style={styles.btnAdd}/>  
+      </TouchableOpacity>
+   
      </View>
       <StatusBar style="auto" />
     </View>
@@ -30,6 +45,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    padding: 10,
   },
   logo: {
     height: 128, 
@@ -38,5 +54,13 @@ const styles = StyleSheet.create({
   btnAdd:{
     width: 32,
     height: 32,
+  },
+  viewInput:{
+   flexDirection: "row",
+   justifyContent: "space-between",
+   width: "100%",
   }
 });
+
+
+
